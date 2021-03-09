c7704c53c0fa470175272923c9bf59954a38ea41
Evgeniya Migunova <8880305@gmail.com>
2021-03-09 16:29:48 -0500

    Add Main.java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int rubles = 40583;
+        int miles = rubles / 20;
+        System.out.println("Количество начисленных миль: " + miles);
+
+    }
+}
